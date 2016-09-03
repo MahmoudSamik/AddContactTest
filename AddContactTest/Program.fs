973490400f2a5a71b460b7236114ac148b934d70
mahmoud <mahmoud-sami@hotmail.com>
2016-09-03 16:32:16 +0300

    double check for adding contact

--- AddContactTest/Program.fs
+++ AddContactTest/Program.fs
@@ -13,8 +13,10 @@ let email="//input[@name='email']"
 let state="//input[@name='state']"
 let contactAdded="//div[@class='toast-message']"
 let dashBoard="//span[@class='title']"
-
-
+let button="//button[@class='btn btn-default-light dropdown-toggle ng-binding']"
+let emptyEmail="//span[@class='help-block ng-binding']"
+let search="//input[@type='search']"
+let verifyAddingContact="//div[@class='dataTables_info']"
 
 
 //this is how you define a test
@@ -29,7 +31,7 @@ let dashBoard="//span[@class='title']"
     click password 
     password << "foobar"
     click "Login"    
-    sleep 9
+    button  == "QA TEMPLATE"
     url "http://sophiafiori.azurewebsites.net/#/contacts"
     click addNewContact
     click firstName
@@ -37,11 +39,15 @@ let dashBoard="//span[@class='title']"
     click lastName 
     lastName << "Sami"
     click email
-    email << "mahmoud-samiiiiiiii@hotmail.com"
+    email << "mahmoud-samiiiiiiiiii@hotmail.com"
     click state
     state << "mmm"
     click "Save"
     contactAdded == "Contact Added Successfully"
+    click search
+    search << "mahmoud-samiiiiiiiiii@hotmail.com"
+    verifyAddingContact=="Showing 1 to 1 of 1 entries"
+
 
 
 //run all tests
